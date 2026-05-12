68bc22b908153bdea24b3d11c16340c5de3ca211
lucasborsolli996811-sketch <lucasborsolli996811@gmail.com>
2026-05-12 20:20:53 -0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,11 @@
+import streamlit as st
+
+st.set_page_config(page_title="Forja App", page_icon="⚒️")
+
+st.title("⚒️ Forja: Projetos e Impressão 3D")
+st.subheader("Bem-vindo ao meu portfólio mobile")
+
+st.write("Aqui você pode conferir meus serviços de desenho técnico e modelagem.")
+
+if st.button("Conhecer Serviços"):
+    st.write("Em breve: Catálogo de peças 3D!")
